8a88bce9d252ead310fd484bd4be8753f2bbfbf8
euglena1215 <teppest1215@gmail.com>
2023-08-24 17:23:04 +0900

    implement type

--- my_active_model/examples/user.rb
+++ my_active_model/examples/user.rb
@@ -3,12 +3,12 @@ require_relative '../lib/my_active_model'
 class User
   include MyActiveModel::Model
 
-  attribute :name
-  attribute :age
+  attribute :name, :string
+  attribute :age, :integer
 end
 
 user = User.new(name: "teppei", age: 26)
-# puts "user.valid?: #{user.valid?}"
+puts "user.valid?: #{user.valid?}"
 puts "user.name: #{user.name}"
 puts "user.age: #{user.age}"
 # puts "user.to_h: #{user.to_h}"
--- my_active_model/lib/my_active_model/model.rb
+++ my_active_model/lib/my_active_model/model.rb
@@ -8,11 +8,42 @@ module MyActiveModel
       end
     end
 
+    def valid?
+      self.class.attributes.each do |name, options|
+        value = send(name)
+        return false unless valid_type?(value, options[:type])
+      end
+
+      true
+    end
+
+    private
+
+    def valid_type?(value, type)
+      case type
+      when :string
+        value.is_a?(String)
+      when :integer
+        value.is_a?(Integer)
+      when :float
+        value.is_a?(Float)
+      when :boolean
+        value.is_a?(TrueClass) || value.is_a?(FalseClass)
+      end
+    end
+
     module ClassMethods
-      def attribute(name)
+      def attribute(name, type)
+        @attributes ||= {}
+        @attributes[name] = { type: type }
+
         class_eval do
           attr_accessor name
         end
+      end
+
+      def attributes
+        @attributes || {}
       end
     end
   end
