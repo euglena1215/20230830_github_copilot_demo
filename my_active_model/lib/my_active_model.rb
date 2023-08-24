3eaa9af26a2ed141d53ac163f011edd0ece4f7e2
euglena1215 <teppest1215@gmail.com>
2023-08-24 15:55:38 +0900

    run bundle gem my_active_model

---
+++ my_active_model/lib/my_active_model.rb
@@ -0,0 +1,6 @@
+require "my_active_model/version"
+
+module MyActiveModel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ my_active_model/lib/my_active_model/version.rb
@@ -0,0 +1,3 @@
+module MyActiveModel
+  VERSION = "0.1.0"
+end
