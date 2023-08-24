531c569aef19d5f1e8cf20d3a71362e3fb6fe8d5
euglena1215 <teppest1215@gmail.com>
2023-08-24 17:23:04 +0900

    implement type

--- my_active_model/examples/user.rb
+++ my_active_model/examples/user.rb
@@ -11,5 +11,5 @@ user = User.new(name: "teppei", age: 26)
 puts "user.valid?: #{user.valid?}"
 puts "user.name: #{user.name}"
 puts "user.age: #{user.age}"
-# puts "user.to_h: #{user.to_h}"
+puts "user.to_h: #{user.to_h}"
 # puts "user.to_json: #{user.to_json}"
