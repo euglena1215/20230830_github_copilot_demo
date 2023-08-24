fd5f2643e7267c8cc01660b9da3464d2442589d5
euglena1215 <teppest1215@gmail.com>
2023-08-24 17:23:04 +0900

    implement type

--- my_active_model/examples/user.rb
+++ my_active_model/examples/user.rb
@@ -3,11 +3,12 @@ require_relative '../lib/my_active_model'
 class User
   include MyActiveModel::Model
 
-  attribute :name
-  attribute :age
+  attribute :name, :string
+  attribute :age, :integer
 end
 
 user = User.new(name: "Jane", age: 30)
+puts "user.valid?: #{user.valid?}"
 puts "user: #{user.inspect}"
 puts "user.name: #{user.name}"
 puts "user.age: #{user.age}"
