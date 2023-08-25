bc4785de6b808c99c1bc6a4be8543e073123be5f
euglena1215 <teppest1215@gmail.com>
2023-08-25 17:00:28 +0900

    Fix user.rb for demo

--- my_active_model/examples/user.rb
+++ my_active_model/examples/user.rb
@@ -7,7 +7,9 @@ class User
   attribute :age
 end
 
-user = User.new(name: "Jane", age: 30)
-puts "user: #{user.inspect}"
+user = User.new(name: "teppei", age: 26)
+# puts "user.valid?: #{user.valid?}"
 puts "user.name: #{user.name}"
 puts "user.age: #{user.age}"
+# puts "user.to_h: #{user.to_h}"
+# puts "user.to_json: #{user.to_json}"
